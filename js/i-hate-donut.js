455fde29acfd7ffa80eb79b8d2614b43932994d2
yisheng90 <yisheng@yishenglee.com>
2016-12-08 11:31:16 +0800

    first try

---
+++ js/i-hate-donut.js
@@ -0,0 +1,27 @@
+var foods = [
+  'Popcorn',
+  'Potato chips',
+  'Shrimp',
+  'Chicken rice',
+  'Poutine',
+  'Tacos',
+  'Toast',
+  'French Toast',
+  'Crab',
+  'Pho',
+  'Lasagna',
+  'Brownie',
+  'Lobster',
+  'Donuts',
+  'Ice cream',
+  'Hamburger',
+  'Sushi',
+  'Chocolate',
+  'Pizza'
+];
+
+var item = foods.indexOf("Donuts");
+console.log('Donuts was ranked ' + item);
+foods.splice(foods.indexOf("Donuts"),1);
+console.log('Donuts is removed.')
+console.log(foods);
